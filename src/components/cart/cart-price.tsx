45408ca2a9067dd4c33948097b9010694390c752
CuongNHT <nhtcit99@gmail.com>
2022-09-22 23:31:39 +0700

    done doc UI and flow

--- src/components/cart/cart-price.tsx
+++ src/components/cart/cart-price.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, useNavigate } from 'react-router-dom';
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { formatCurrency } from 'src/utils/currency';
 import CourseService from 'src/services/course';
 import { Modal } from 'antd';
@@ -8,6 +8,8 @@ import RoutePaths from 'src/utils/routes';
 import { useAppDispatch } from 'src/apps/hooks';
 import { cartActions } from 'src/reducers/document/documentSlice';
 import AppButton from 'src/components/button';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 interface ChildProps {
 	data: number;
 	docNum: number;
@@ -20,7 +22,7 @@ const PricingCard: React.FC<ChildProps> = ({
 	children,
 	visible,
 }) => {
-	const [btnText, setBtnText] = useState<string>('CHECKOUT');
+	const [btnText, setBtnText] = useState<string>('Thanh toán');
 	const [open, setOpen] = useState(false);
 	const [confirmLoading, setConfirmLoading] = useState(false);
 	const [modalText, setModalText] = useState('Content of the modal');
@@ -52,65 +54,68 @@ const PricingCard: React.FC<ChildProps> = ({
 			console.log(error);
 		}
 	};
-
+	useEffect(() => {
+		if (docNum === 0) {
+			setBtnText('Tiếp tục chọn tài liệu');
+		} else {
+			setBtnText('Thanh toán');
+		}
+	}, [docNum]);
 	const handleCancel = () => {
 		console.log('Clicked cancel button');
 		setOpen(false);
 	};
 	return (
-		<div className="card border-0 shadow-sm">
-			<div className="card-body">
-				<div className="vstack gap-2">
-					{/* <div className="d-flex justify-content-between">
-						<span>Subtotal:</span>
-						<span>30,000Ks</span>
-					</div>
-					<div className="d-flex justify-content-between">
-						<span>Discount:</span>
-						<span className="text-danger">-</span>
-					</div>
-					<div className="d-flex justify-content-between">
-						<span>Delivery charge:</span>
-						<span className="text-success">+2,000Ks</span>
-					</div> */}
-					<Modal
-						title="Xác nhận đặt hàng"
-						visible={open}
-						onOk={handleOk}
-						confirmLoading={confirmLoading}
-						onCancel={handleCancel}
+		<div
+			className=""
+			css={css`
+				.ant-btn {
+					letter-spacing: 4px;
+					&:hover {
+						letter-spacing: 6px;
+					}
+				}
+				a.ant-btn {
+					padding-top: 8px !important;
+				}
+			`}
+		>
+			<Modal
+				title="Xác nhận đặt hàng"
+				visible={open}
+				onOk={handleOk}
+				confirmLoading={confirmLoading}
+				onCancel={handleCancel}
+			>
+				<p>
+					Xác nhận đặt đơn hàng trị giá:
+					<strong>{`${formatCurrency(data)}`}</strong>
+				</p>
+			</Modal>
+			<hr className="text-muted" />
+
+			<div className="d-flex justify-content-between">
+				<span className="h5">Total:</span>
+				<span className="fw-bold h5 mb-0">{formatCurrency(data)}</span>
+			</div>
+
+			{
+				<div className="d-grid gap-2 mt-2">
+					<AppButton
+						btnTextColor={'black'}
+						btnStyle={'outline'}
+						btnSize={'small'}
+						btnWidth={'full-w'}
+						onClick={showModal}
+						href={docNum === 0 ? RoutePaths.DOCUMENT : ''}
 					>
-						<p>
-							Xác nhận đặt đơn hàng trị giá:
-							<strong>{`${formatCurrency(data)}`}</strong>
-						</p>
-					</Modal>
-					<hr className="text-muted" />
-
-					<div className="d-flex justify-content-between">
-						<span className="h5">Total:</span>
-						<span className="fw-bold h5 mb-0">{formatCurrency(data)}</span>
-					</div>
-
-					{
-						<div className="d-grid gap-2 mt-2">
-							<AppButton
-								btnTextColor={'black'}
-								btnStyle={'outline'}
-								btnSize={'small'}
-								btnWidth={'full-w'}
-								onClick={showModal}
-								disabled={btnText === 'CREATED' || docNum === 0}
-							>
-								{btnText}
-							</AppButton>
-							{/* <Link to="/">
+						{btnText}
+					</AppButton>
+					{/* <Link to="/">
 								<div className="btn btn-outline-primary">Continue Shopping</div>
 							</Link> */}
-						</div>
-					}
 				</div>
-			</div>
+			}
 		</div>
 	);
 };
