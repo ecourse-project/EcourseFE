84d1ba934ca5470d9d8e844739739fab7072ed77
cuongnht <nhtcit99@gmail.com>
2025-04-29 12:55:41 +0700

    enhance: add minimum loading time hook

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,12 +1,22 @@
+import React, { ReactNode } from 'react';
 import Banner from '../navbar/banner';
 import Navbar from '../navbar/nav-bar';
 
-const Layout = ({ children, isNoneHeader = false }) => (
-  <>
-    <div>{!isNoneHeader && <Banner />}</div>
-    <div>{!isNoneHeader && <Navbar />}</div>
-    <div className="page-container">{children}</div>
-  </>
+interface LayoutProps {
+  children: ReactNode;
+  isNoneHeader?: boolean;
+}
+
+const Layout = React.memo(
+  ({ children, isNoneHeader = false }: LayoutProps) => (
+    <>
+      <div>{!isNoneHeader && <Banner />}</div>
+      <div>{!isNoneHeader && <Navbar />}</div>
+      <div className="page-container">{children}</div>
+    </>
+  ),
+  (prevProps, nextProps) =>
+    prevProps.isNoneHeader === nextProps.isNoneHeader && prevProps.children === nextProps.children,
 );
 
 export default Layout;
