7d86a1d000f867079ecf2cbf597e1e9b61cf1f5c
Cuong Nguyen <76060281+nhtc@users.noreply.github.com>
2025-06-13 19:11:46 +0000

    edit url endpoint

--- src/lib/config/env.ts
+++ src/lib/config/env.ts
@@ -1,5 +1,5 @@
 const globalVariable = {
-  API_URL: 'http://be.teachingchem.edu.vn/',
+  API_URL: 'https://be.teachingchem.edu.vn/',
   // API_URL: 'http://localhost:4000/',
   GMAIL_URL: 'https:mail.google.com/mail/u/0/?fs=1&to=creativeteaching.net@gmail.com&su=&body=&tf=cm',
 };
